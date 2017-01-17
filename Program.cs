94516b3f427010584b9bea39b75bc338a80acd65
sindhu789 <sindhuvelumula@ksu.edu>
2017-01-16 21:33:48 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Intro_VS_GitHub
+{
+    class Program
+    {
+
+        // Welcomes the user, you need to add ", to CIS50"1:
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World");
+
+            // retain command window till user presses Enter
+            Console.ReadLine();
+        }
+    }
+}
